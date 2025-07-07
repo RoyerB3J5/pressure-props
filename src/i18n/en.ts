73f60314728e6f9e5b2be857677177cce60eb2c9
Royer Bonifacio <royer.boni13@gmail.com>
2025-07-07 15:39:46 -0500

    First commit

---
+++ src/i18n/en.ts
@@ -0,0 +1,80 @@
+export default {
+  header: {
+    nav: [
+      {
+        label: "Home",
+        href:"/"
+      },
+      {
+        label: "Type of Services",
+        href:"/"
+      },
+      {
+        label: "Gallery",
+        href:"/"
+      },
+      {
+        label: "About Us",
+        href:"/"
+      },
+      {
+        label: "Contact Us",
+        href:"/"
+      },
+    ],
+    button: "Get Estimate",
+    phone : "CALL US"
+  },
+  main:{
+    hero:{
+      title:"Luxury Finished Low Maintenance <span class='text-primary-light'>Yards</span> Design And Installation",
+      button1:"Type of Gardens",
+      button2:"Get an Estimate",
+      lists:[
+        {
+          title:"Consultation",
+          description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          icon:"consultation"
+        },
+        {
+          title:"Design",
+          description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          icon:"design"
+        },
+        {
+          title:"Execution",
+          description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          icon:"execution"
+        },
+      ]
+    }
+  },
+  footer:{
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing e,elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    hours: "SERVICE HOURS",
+    call: "CALL US",
+    links_title: "QUICK LINKS",
+    links: [
+      {
+        label: "Home",
+        href:"/"
+      },
+      {
+        label: "Type of Services",
+        href:"/"
+      },
+      {
+        label: "Gallery",
+        href:"/"
+      },
+      {
+        label: "About Us",
+        href:"/"
+      },
+      {
+        label: "Contact Us",
+        href:"/"
+      },
+    ],
+  }
+}
---
+++ src/i18n/es.ts
@@ -0,0 +1,84 @@
+export default {
+  header: {
+    nav: [
+      {
+        label: 'Inicio',
+        href: '/',
+      },
+      {
+        label: 'Tipos Servicios',
+        href: '/',
+      },
+      {
+        label: 'Galería',
+        href: '/',
+      },
+      {
+        label: 'Nosotros',
+        href: '/',
+      },
+      {
+        label: 'Contáctanos',
+        href: '/',
+      },
+    ],
+    button: 'Cotización',
+    phone: 'LLÁMANOS',
+  },
+  main: {
+    hero: {
+      title:
+        "Diseño e Instalación de <span class='text-primary-light'>Jardines</span> de Lujo de Bajo Mantenimiento",
+      button1: 'Tipos de Jardines',
+      button2: 'Obtener Cotización',
+      lists: [
+        {
+          title: 'Consulta',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          icon: 'consultation',
+        },
+        {
+          title: 'Diseño',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          icon: 'design',
+        },
+        {
+          title: 'Ejecución',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          icon: 'execution',
+        },
+      ],
+    },
+  },
+  footer: {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    hours: 'HORARIOS DE SERVICIO',
+    call: 'LLÁMANOS',
+    links_title: 'LINKS',
+    links: [
+      {
+        label: 'Inicio',
+        href: '/',
+      },
+      {
+        label: 'Tipos de Servicios',
+        href: '/',
+      },
+      {
+        label: 'Galería',
+        href: '/',
+      },
+      {
+        label: 'Nosotros',
+        href: '/',
+      },
+      {
+        label: 'Contáctanos',
+        href: '/',
+      },
+    ],
+  },
+};
