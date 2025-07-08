535faf725f60118d134cbaed735baded2ffd9f81
Royer Bonifacio <royer.boni13@gmail.com>
2025-07-07 22:45:40 -0500

    add:two first sections

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -2,79 +2,136 @@ export default {
   header: {
     nav: [
       {
-        label: "Home",
-        href:"/"
+        label: 'Home',
+        href: '/',
       },
       {
-        label: "Type of Services",
-        href:"/"
+        label: 'Type of Services',
+        href: '/',
       },
       {
-        label: "Gallery",
-        href:"/"
+        label: 'Gallery',
+        href: '/',
       },
       {
-        label: "About Us",
-        href:"/"
+        label: 'About Us',
+        href: '/',
       },
       {
-        label: "Contact Us",
-        href:"/"
+        label: 'Contact Us',
+        href: '/',
       },
     ],
-    button: "Get Estimate",
-    phone : "CALL US"
+    button: 'Get Estimate',
+    phone: 'CALL US',
   },
-  main:{
-    hero:{
-      title:"Luxury Finished Low Maintenance <span class='text-primary-light'>Yards</span> Design And Installation",
-      button1:"Type of Gardens",
-      button2:"Get an Estimate",
-      lists:[
+  main: {
+    hero: {
+      title:
+        "Luxury Finished Low Maintenance <span class='text-primary-light'>Yards</span> Design And Installation",
+      button1: 'Type of Gardens',
+      button2: 'Get an Estimate',
+      lists: [
         {
-          title:"Consultation",
-          description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          icon:"consultation"
+          title: 'Consultation',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          icon: 'consultation',
         },
         {
-          title:"Design",
-          description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          icon:"design"
+          title: 'Design',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          icon: 'design',
         },
         {
-          title:"Execution",
-          description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          icon:"execution"
+          title: 'Execution',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          icon: 'execution',
         },
-      ]
-    }
+      ],
+    },
+    experience: {
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      description2:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      items: [
+        'We are expert gardeners',
+        'Expert team members',
+        'Afordable quality services',
+      ],
+      years: 'YEARS OF  EXPERIENCE',
+      button: 'LEARN MORE',
+    },
+    solutions: {
+      description:
+        "We offer a  comprehensive range of <span class='font-semibold'>Landscaping</span> services including",
+      title: 'Hardscape Solutions',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      items: [
+        {
+          title: 'Paver Patios and Paver Sealing',
+          image: 'patios',
+        },
+        {
+          title: 'Retainer Walls',
+          image: 'retainer',
+        },
+        {
+          title: 'Walkways and Diveway Extensions',
+          image: 'wakways',
+        },
+        {
+          title: 'Firepits',
+          image: 'firepits',
+        },
+        {
+          title: 'Drainage',
+          image: 'drainage',
+        },
+        {
+          title: 'Turn and Artificial Grass',
+          image: 'turn',
+        },
+        {
+          title: 'Pool Areas Landscape',
+          image: 'pool',
+        },
+        {
+          title: 'Pergolas',
+          image: 'pergolas',
+        },
+      ],
+    },
   },
-  footer:{
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing e,elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    hours: "SERVICE HOURS",
-    call: "CALL US",
-    links_title: "QUICK LINKS",
+  footer: {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing e,elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    hours: 'SERVICE HOURS',
+    call: 'CALL US',
+    links_title: 'QUICK LINKS',
     links: [
       {
-        label: "Home",
-        href:"/"
+        label: 'Home',
+        href: '/',
       },
       {
-        label: "Type of Services",
-        href:"/"
+        label: 'Type of Services',
+        href: '/',
       },
       {
-        label: "Gallery",
-        href:"/"
+        label: 'Gallery',
+        href: '/',
       },
       {
-        label: "About Us",
-        href:"/"
+        label: 'About Us',
+        href: '/',
       },
       {
-        label: "Contact Us",
-        href:"/"
+        label: 'Contact Us',
+        href: '/',
       },
     ],
-  }
-}
+  },
+};
--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -52,6 +52,57 @@ export default {
         },
       ],
     },
+    experience: {
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  description2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  items: [
+    "Somos jardineros expertos",
+    "Miembros del equipo con experiencia",
+    "Servicios de calidad a precios accesibles"
+  ],
+  years: "AÑOS DE EXPERIENCIA",
+  button: "SABER MÁS"
+},
+solutions: {
+  description: "Ofrecemos una amplia gama de servicios de <span class='font-semibold'>paisajismo</span>, incluyendo",
+  title: "Soluciones de Hardscape",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  items: [
+    {
+      title: "Patios con adoquines y sellado",
+      image: "patios"
+    },
+    {
+      title: "Muros de contención",
+      image: "retainer"
+    },
+    {
+      title: "Caminos y extensiones de entrada",
+      image: "wakways"
+    },
+    {
+      title: "Fogatas",
+      image: "firepits"
+    },
+    {
+      title: "Drenaje",
+      image: "drainage"
+    },
+    {
+      title: "Césped natural y artificial",
+      image: "turn"
+    },
+    {
+      title: "Paisajismo en áreas de piscina",
+      image: "pool"
+    },
+    {
+      title: "Pérgolas",
+      image: "pergolas"
+    }
+  ]
+}
+
   },
   footer: {
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
